594a5ea3b9e2f3954afefe1f793b682af58b0c2a
Priyavm-Mani <priyavmofficial@gmail.com>
2025-07-25 15:42:06 +0530

    Add SELECT queries from learning resources

---
+++ learning resource/select.sql
@@ -0,0 +1,4 @@
+ select * from sales;
+ select saledate,customers,amount from sales;
+ select saledate,amount,boxes,amount/boxes from sales;
+ select saledate,amount,boxes,amount/boxes 'amount per box' from sales;
